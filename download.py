7b756c6c54f4b1066896365b095de2ee3031c419
luiscnr <lois@LAPTOP-SU04997U.localdomain>
2023-02-20 09:51:53 +0100

    Added download...

--- download.py
+++ download.py
@@ -26,7 +26,7 @@ def main():
         outputdir = '/mnt/c/DATA_LUIS/OCTAC_WORK/ARC_TEST'
         products, product_names, collection_id = edac.search_olci_by_point('2023-01-19', 'FR', 'L2', 45.324091, 12.527398
                                                                        , -1, -1)
-        edac.download_product_from_product_list(products, outputdir)
+        #edac.download_product_from_product_list(products, outputdir)
 
 def checkpy():
     valid = True
